44fb0c260d810b12875e7b7109cebfc1d37be7fe
JDeuce <jjaques@gmail.com>
2017-12-09 00:46:02 -0600

    initial commit

---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+	public CinemachineVirtualCamera closeCam;
+	public CinemachineVirtualCamera farCam;
+
+	public int activePriority = 11;
+	public int inactivePriority = 10;
+
+	public SpriteRenderer player;
+	public Transform wall;
+
+	public float FarThreshold = 5;
+	public float Distance; // for easy inspector access
+
+	// Update is called once per frame
+	void Update () {
+
+		Distance = Vector2.Distance(player.transform.position, wall.position);
+
+		if (Distance > FarThreshold) {
+			farCam.m_Priority = activePriority;
+			closeCam.m_Priority = inactivePriority;
+		} else {
+			closeCam.m_Priority = activePriority;
+			farCam.m_Priority = inactivePriority;
+		}
+	}
+}
