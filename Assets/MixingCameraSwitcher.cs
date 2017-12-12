12568b9cf323f696a2f85700a2ba2567db751942
JDeuce <jjaques@gmail.com>
2017-12-11 21:52:42 -0600

    add a mixing camera test, and make everything easier to see

---
+++ Assets/MixingCameraSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MixingCameraSwitcher : MonoBehaviour {
+
+	public float transitionTime = 2;
+	public SpriteRenderer player;
+	public Transform wall;
+
+	public float FarThreshold = 5;
+	public float Distance; // for easy inspector access
+
+
+	private bool isFar;
+
+	private CinemachineMixingCamera mixingCamera;
+
+	void Start() {
+		mixingCamera = GetComponent<CinemachineMixingCamera>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Distance = Vector2.Distance(player.transform.position, wall.position);
+
+		isFar = Distance > FarThreshold;
+
+		float rate = Time.deltaTime / transitionTime;
+
+		if (Distance > FarThreshold) {
+			mixingCamera.m_Weight0 = Mathf.Clamp01(mixingCamera.m_Weight0 - rate);
+			mixingCamera.m_Weight1 = Mathf.Clamp01(mixingCamera.m_Weight1 + rate);
+		} else {
+			mixingCamera.m_Weight1 = Mathf.Clamp01(mixingCamera.m_Weight1 - rate);
+			mixingCamera.m_Weight0 = Mathf.Clamp01(mixingCamera.m_Weight0 + rate);
+		}
+	}
+}
